b26c14c4ac0600be6dbe94aef4c6ae85b3ec4bba
Vidyasagar Thaniparti <vidyasagar@certifyglobal.com>
2020-05-24 19:10:09 +0530

    ensure debug/warn logs are pushed to app insights as events!

--- app/src/main/java/com/certify/snap/common/Logger.java
+++ app/src/main/java/com/certify/snap/common/Logger.java
@@ -11,13 +11,14 @@ import java.util.Map;
 
 
 public class Logger {
-    private static Map<String, String> properties = new HashMap<String, String>(){{
-       put("sn", Util.getSerialNumber());
+    private static Map<String, String> properties = new HashMap<String, String>() {{
+        put("sn", Util.getSerialNumber());
     }};
+
     public static void debug(String classname, String message) {
 
         Log.d(classname, "" + message);
-        Analytics.trackEvent(message, properties );
+        Analytics.trackEvent(classname+" "+message, properties);
     }
 
     public static void toast(Context context, String message) {
@@ -25,16 +26,12 @@ public class Logger {
     }
 
     public static void error(String classname, String message) {
-        if (EndPoints.deployment == EndPoints.Mode.Demo) {
-            if (message == null) message = "null";
-            Log.e(classname,message);
-            Analytics.trackEvent("Date Time"+Util.getMMDDYYYYDate()+"SN"+Util.getSNCode()+"Error:"+message);
-        }else{
-            Analytics.trackEvent("Date Time"+Util.getMMDDYYYYDate()+"SN"+Util.getSNCode()+"Error:"+message);
-        }
-
+        Log.e(classname, message);
+        Analytics.trackEvent(classname+" "+message, properties);
     }
-    public static void warn(String tag, String message){
+
+    public static void warn(String tag, String message) {
         Log.w(tag, message);
+        Analytics.trackEvent(message, properties);
     }
 }
