3e60a7bf4751e9a8bfec1907e2b39b10155b9792
dbhimalli <deepak.bhimalli@certifyglobal.com>
2020-06-20 19:09:18 -0400

    Excluded the Facial setting if the Home Screen view setting is disabled

--- app/src/main/java/com/certify/snap/activity/ThermalSetting.java
+++ app/src/main/java/com/certify/snap/activity/ThermalSetting.java
@@ -107,12 +107,13 @@ public class ThermalSetting extends Activity {
         }
 
     }
-public void  setIdentificationOptions(){
-    Util.writeBoolean(sp, GlobalParameters.QR_SCREEN, false);
-    Util.writeBoolean(sp, GlobalParameters.ANONYMOUS_ENABLE, false);
-    Util.writeBoolean(sp, GlobalParameters.RFID_ENABLE, false);
-    Util.writeBoolean(sp, GlobalParameters.FACIAL_DETECT, false);
-}
+
+    public void  setIdentificationOptions() {
+        Util.writeBoolean(sp, GlobalParameters.QR_SCREEN, false);
+        Util.writeBoolean(sp, GlobalParameters.ANONYMOUS_ENABLE, false);
+        Util.writeBoolean(sp, GlobalParameters.RFID_ENABLE, false);
+    }
+
     public void onParamterback(View view) {
         startActivity(new Intent(ThermalSetting.this, SettingActivity.class));
         finish();
