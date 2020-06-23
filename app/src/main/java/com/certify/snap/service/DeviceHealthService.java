da3fcd486f5eb53a6a0cf22e4da563f69c11bb0b
narayana <narayana@etilaaf.com>
2020-06-24 00:32:54 +0530

    Guide Settings UI save data,DeviceSettings Data management UI click action done

--- app/src/main/java/com/certify/snap/service/DeviceHealthService.java
+++ app/src/main/java/com/certify/snap/service/DeviceHealthService.java
@@ -20,66 +20,65 @@ import java.util.Calendar;
 import static android.os.SystemClock.elapsedRealtime;
 
 public class DeviceHealthService extends Service implements JSONObjectCallback {
-   protected static final String LOG = "BackgroundSyncService - ";
-   private final static int BACKGROUND_INTERVAL_10_MINUTES = 10;
-   private AlarmManager alarmService;
-   private PendingIntent restartServicePendingIntent;
+    protected static final String LOG = "BackgroundSyncService - ";
+    private final static int BACKGROUND_INTERVAL_10_MINUTES = 10;
+    private AlarmManager alarmService;
+    private PendingIntent restartServicePendingIntent;
 
-   @Override
-   public IBinder onBind(Intent intent) {
-      return null;
-   }
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
-   @RequiresApi(api = Build.VERSION_CODES.M)
-   @Override
-   public int onStartCommand(Intent intent, int flags, int startId) {
-     // Toast.makeText(this, "Service started by user.", Toast.LENGTH_LONG).show();
-      try {
-         //sendNotificationEvent(getString(R.string.app_name), "Alert Background MyRabbit", "", getApplicationContext());
-         restartServicePendingIntent = PendingIntent.getService(getApplicationContext(), 1, new Intent(this, DeviceHealthService.class), PendingIntent.FLAG_ONE_SHOT);
-         alarmService = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-         Calendar cal = Calendar.getInstance();
-         long sysTime = elapsedRealtime();
-         cal.set(Calendar.MINUTE, cal.get(Calendar.MINUTE) + (BACKGROUND_INTERVAL_10_MINUTES - (cal.get(Calendar.MINUTE) % BACKGROUND_INTERVAL_10_MINUTES)));
-         cal.set(Calendar.SECOND, 0);
-         cal.set(Calendar.MILLISECOND, 0);
-         long currTime = Util.getCurrentTimeLong();
-         if (alarmService != null)
-            alarmService.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, sysTime + (cal.getTimeInMillis() - currTime), restartServicePendingIntent);
-           Util.getDeviceHealthCheck(this,this);
-      } catch (Exception e) {
-         Logger.error(LOG + "onStartCommand(Intent intent, int flags, int startId)", e.getMessage());
-      }
-      return super.onStartCommand(intent, flags, startId);
-   }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Toast.makeText(this, "Service started by user.", Toast.LENGTH_LONG).show();
+        try {
+            //sendNotificationEvent(getString(R.string.app_name), "Alert Background MyRabbit", "", getApplicationContext());
+            restartServicePendingIntent = PendingIntent.getService(getApplicationContext(), 1, new Intent(this, DeviceHealthService.class), PendingIntent.FLAG_ONE_SHOT);
+            alarmService = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+            Calendar cal = Calendar.getInstance();
+            long sysTime = elapsedRealtime();
+            cal.set(Calendar.MINUTE, cal.get(Calendar.MINUTE) + (BACKGROUND_INTERVAL_10_MINUTES - (cal.get(Calendar.MINUTE) % BACKGROUND_INTERVAL_10_MINUTES)));
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+            long currTime = Util.getCurrentTimeLong();
+            if (alarmService != null)
+                alarmService.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, sysTime + (cal.getTimeInMillis() - currTime), restartServicePendingIntent);
+            Util.getDeviceHealthCheck(this, this);
+        } catch (Exception e) {
+            Logger.error(LOG + "onStartCommand(Intent intent, int flags, int startId)", e.getMessage());
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
 
-   @Override
-   public void onDestroy() {
-      super.onDestroy();
-      if (alarmService != null && restartServicePendingIntent != null) {
-         alarmService.cancel(restartServicePendingIntent);
-      }
-   }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (alarmService != null && restartServicePendingIntent != null) {
+            alarmService.cancel(restartServicePendingIntent);
+        }
+    }
 
-   @Override
-   public void onJSONObjectListener(String reportInfo, String status, JSONObject req) {
-      //do noop
-      try {
-         if (reportInfo == null) {
-            return;
-         }
+    @Override
+    public void onJSONObjectListener(String reportInfo, String status, JSONObject req) {
+        //do noop
+        try {
+            if (reportInfo == null) {
+                return;
+            }
 
-         if(reportInfo.contains("token expired"))
-            Util.getToken(this,this);
+            if (reportInfo.contains("token expired"))
+                Util.getToken(this, this);
+        } catch (Exception e) {
+            Logger.error("onJSONObjectListener(JSONObject reportInfo, String status, JSONObject req)", e.getMessage());
+        }
+    }
 
-      } catch (Exception e) {
-         Logger.error("onJSONObjectListener(JSONObject reportInfo, String status, JSONObject req)", e.getMessage());
-      }
-   }
-
-   @Override
-   public void onTaskRemoved(Intent rootIntent) {
-      super.onTaskRemoved(rootIntent);
-      stopSelf();
-   }
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+        stopSelf();
+    }
 }
