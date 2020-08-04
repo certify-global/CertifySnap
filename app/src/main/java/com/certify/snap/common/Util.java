046b78287708a6212a3c0a684005aca08289e462
NagaK <nagarjuna.kottam@certifyglobal.com>
2020-08-04 13:50:55 -0400

    Fixed while restart  App going to Login screen with password

--- app/src/main/java/com/certify/snap/common/Util.java
+++ app/src/main/java/com/certify/snap/common/Util.java
@@ -1882,7 +1882,6 @@ public class Util {
 
     public static void restartApp(Context context) {
         stopMemberSyncService(context);
-        Application.getInstance().exit();
         Intent intent = new Intent(context, GuideActivity.class);
         int mPendingIntentId = 111111;
         PendingIntent mPendingIntent = PendingIntent.getActivity(context, mPendingIntentId, intent, PendingIntent.FLAG_CANCEL_CURRENT);
