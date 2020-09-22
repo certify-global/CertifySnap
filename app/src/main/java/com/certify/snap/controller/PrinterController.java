1178ebf80306467b5a236b2a130d43138cf0bdce
dbhimalli <deepak.bhimalli@certifyglobal.com>
2020-09-22 19:31:12 -0400

    UsbPrint: Clear usb connection data when the activity is getting destroyed

--- app/src/main/java/com/certify/snap/controller/PrinterController.java
+++ app/src/main/java/com/certify/snap/controller/PrinterController.java
@@ -38,9 +38,9 @@ public class PrinterController implements BCPControl.LIBBcpControlCallBack {
     private PrinterMsgHandle mHandle;
     private PrinterMsgDialog mDialog;
     private Context context;
-    private PrintData mPrintData = new PrintData();
+    private PrintData mPrintData = null;
     private BCPControl mUsbPrintControl = null;
-    private ConnectionData mConnectData = new ConnectionData();
+    private ConnectionData mConnectData = null;
     private ConnectionDelegate mConnectionDelegate = null;
     private PrintDialogDelegate mPrintDialogDelegate = null;
     private int mCurrentIssueMode = AsynchronousMode;
@@ -149,6 +149,8 @@ public class PrinterController implements BCPControl.LIBBcpControlCallBack {
     }
 
     private void initUsbPrint() {
+        mPrintData = new PrintData();
+        mConnectData = new ConnectionData();
         String item = "B-FV4D";
         util.setPreferences(context, PRINTER_TYPE_KEYNAME, item);
         util.setPreferences(context, PORTSETTING_PORT_MODE_KEYNAME, "FILE");
