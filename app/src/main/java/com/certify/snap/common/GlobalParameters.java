9f26922e0253dc9bfc9a6e3e754a45ea68e21a9d
dbhimalli <deepak.bhimalli@certifyglobal.com>
2020-11-02 09:13:02 -0500

    Scan: Added Scan type settings

--- app/src/main/java/com/certify/snap/common/GlobalParameters.java
+++ app/src/main/java/com/certify/snap/common/GlobalParameters.java
@@ -45,6 +45,7 @@ public class GlobalParameters {
     public static final String ScanMode = "ScanMode";
     public static final String ScanProximity = "ScanProximity";
     public static final String PRAGMA_KEY = "pragmakey";
+    public static final String ScanType = "ScanType";
 
     public static String Access_token = "";
     public static String channelID = "";
