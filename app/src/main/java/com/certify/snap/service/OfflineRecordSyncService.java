662b745e736d5373fc7dbbdce96d1efb3c1cfc27
dbhimalli <deepak.bhimalli@certifyglobal.com>
2021-05-03 17:44:43 -0400

    Added timezone for AccessLog API

--- app/src/main/java/com/certify/snap/service/OfflineRecordSyncService.java
+++ app/src/main/java/com/certify/snap/service/OfflineRecordSyncService.java
@@ -296,6 +296,9 @@ public class OfflineRecordSyncService extends Service implements RecordTemperatu
             jsonObject.put("locationName", json.getString("locationName"));
 
             jsonObject.put("deviceTime", json.getString("deviceTime"));
+            if (json.has("timezone")) {
+                jsonObject.put("timezone", json.getString("timezone"));
+            }
             jsonObject.put("deviceData", json.getString("deviceData"));
             jsonObject.put("guid", json.getString("guid"));
             jsonObject.put("faceParameters", json.getString("faceParameters"));
