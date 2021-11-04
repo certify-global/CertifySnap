7a93168f86b88db818a8c49c72a5bf8ebd17790a
nbijivemula <narayana.bijivemula@certifyglobal.com>
2021-11-04 18:20:21 +0530

    bug and Enabling touch mode option UI and API

--- app/src/main/java/com/certify/snap/common/Util.java
+++ app/src/main/java/com/certify/snap/common/Util.java
@@ -1817,6 +1817,7 @@ public class Util {
             } else {
                 obj.put("groupId", "0");
             }
+            obj.put ("fromDate", Util.getUTCDate(""));
             new AsyncJSONObjectGetMemberList(obj, callback, sharedPreferences.getString(GlobalParameters.URL,
                     EndPoints.prod_url) + EndPoints.GetMemberList, context).execute();
 
