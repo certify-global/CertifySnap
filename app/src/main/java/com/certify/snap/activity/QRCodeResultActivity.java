9e078dc2aaf31404b6d6c69f2517797d16dcdec5
narayana20 <narayana.bijivemula@certifyglobal.com>
2022-12-01 12:09:59 +0530

    Duplicate library recods removed

--- app/src/main/java/com/certify/snap/activity/QRCodeResultActivity.java
+++ app/src/main/java/com/certify/snap/activity/QRCodeResultActivity.java
@@ -1,45 +1,17 @@
 package com.certify.snap.activity;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.certify.callback.FlowListCallback;
 import com.certify.snap.R;
-import com.certify.snap.api.response.TouchlessSettings;
-import com.certify.snap.async.AsyncJSONObjectFlowList;
-import com.certify.snap.common.AppSettings;
-import com.certify.snap.common.EndPoints;
-import com.certify.snap.common.GlobalParameters;
-import com.certify.snap.common.Util;
-import com.certify.snap.controller.DatabaseController;
-import com.certify.snap.controller.DeviceSettingsController;
-import com.certify.snap.controller.GestureController;
 import com.certify.snap.qrverification.CertificateModel;
-import com.certify.snap.qrverification.QRModelData;
-import com.google.android.material.textfield.TextInputLayout;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class QRCodeResultActivity extends AppCompatActivity {
     public static String TAG = "QRCodeResultActivity";
--- app/src/main/java/com/certify/snap/qrverification/VerificationFragment.kt
+++ app/src/main/java/com/certify/snap/qrverification/VerificationFragment.kt
@@ -22,23 +22,15 @@
 
 package com.certify.snap.qrverification
 
-import android.app.Dialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
 import androidx.core.os.bundleOf
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.fragment.navArgs
 import com.certify.snap.databinding.FragmentVerificationBinding
-import dagger.hilt.android.AndroidEntryPoint
 import dgca.verifier.app.android.model.rules.RuleValidationResultModelsContainer
 
 class VerificationFragment : Fragment() {
